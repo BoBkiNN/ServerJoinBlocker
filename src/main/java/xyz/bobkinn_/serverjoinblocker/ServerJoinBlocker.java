9261b197c1195ec41927607ae0ca8ff161a84fc3
BoBkiNN_ <bobkin.yangirov@gmail.com>
2022-05-08 17:17:04 +0600

    update

--- src/main/java/xyz/bobkinn_/serverjoinblocker/ServerJoinBlocker.java
+++ src/main/java/xyz/bobkinn_/serverjoinblocker/ServerJoinBlocker.java
@@ -1,13 +1,13 @@
 package xyz.bobkinn_.serverjoinblocker;
 
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.event.PreLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
 import net.md_5.bungee.event.EventHandler;
-import net.md_5.bungee.api.event.PreLoginEvent;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,36 +16,41 @@ public final class ServerJoinBlocker extends Plugin implements Listener {
     public static File configFile;
     public static Configuration configuration;
     public File dataFolder = getDataFolder();
-    public static File switchFile;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getLogger().info("[SJB] Starting...");
+        getLogger().info("Starting...");
         String[] bEnableAliases= {"blockjoinenable","bon"};
         String[] bDisableAliases={"blockjoindisable","boff"};
         configLoad();
 
         getProxy().getPluginManager().registerCommand(this, new CommandEnable("benable","serverjoinblocker.enable", bEnableAliases, dataFolder));
         getProxy().getPluginManager().registerCommand(this, new CommandDisable("bdisable","serverjoinblocker.disable", bDisableAliases, dataFolder));
+        getProxy().getPluginManager().registerListener(this,this);
+        getLogger().info("Started!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("Disabled!");
     }
+
 
     @EventHandler
     public void onConnect(PreLoginEvent e) {
         boolean blockEnabled = configuration.getBoolean("blockEnabled");
         String kickMsg = configuration.getString("kickMsg").replace("&","§");
-        if (!e.isCancelled()){
-            if (blockEnabled){
-                e.setCancelled(true);
-                e.setCancelReason(new ComponentBuilder (kickMsg).create());
-            }
+
+        if (blockEnabled){
+            e.getConnection().disconnect(new ComponentBuilder (kickMsg).create());
+            e.setCancelled(true);
+            e.setCancelReason(new ComponentBuilder (kickMsg).create());
         }
+
     }
+
 
     private void configLoad() {
         if(!getDataFolder().exists()){
